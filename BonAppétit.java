8187f33e41496b951d270ac9aac92cf801a75f93
ambatisandeep <61520051+ambatisandeep@users.noreply.github.com>
2020-09-03 03:01:38 +0530

    Bon Appétit
    https://www.hackerrank.com/challenges/bon-appetit/problem

---
+++ BonAppétit.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int k, int b) {
+
+        int sum=0;
+        int change = 0;
+        for(int i = 0 ; i<bill.size();i++){
+            if(k!=i){
+                sum=sum+bill.get(i);
+            } 
+        }
+        int share = sum/2;
+
+        if(share==b){
+            System.out.println("Bon Appetit");
+        }else{
+           System.out.println(b-share);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
