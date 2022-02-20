17e7c9b4322a15386744a6697a7b3e7f35d41176
Pavan Kumar TULASI <pavan.kumar.tulasi@ing.com>
2022-02-20 16:07:41 +0100

    modifed the class name

--- src/com/practice/UtopianTree.Java
+++ src/com/practice/UtopianTree.Java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class UtopianTree {
 
     // Complete the utopianTree function below.
     static int utopianTree(int n) {
