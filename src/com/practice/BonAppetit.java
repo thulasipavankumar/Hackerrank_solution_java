eef52203a534313a37d9d0ee436c6d6959b130a3
Pavan Kumar TULASI <pavan.kumar.tulasi@ing.com>
2022-02-26 18:08:34 +0100

    removed unsed imports

--- src/com/practice/BonAppetit.java
+++ src/com/practice/BonAppetit.java
@@ -1,17 +1,12 @@
 package com.practice;
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
+
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
+
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-public class BonAppétit {
+public class BonAppetit {
 
     // Complete the bonAppetit function below.
     static void bonAppetit(List<Integer> bill, int k, int b) {
